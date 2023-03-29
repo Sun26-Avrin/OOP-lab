95a92a9150615f2130e0f27a3bab6df11ec041c1
Ping9 <sim2626@naver.com>
2023-03-29 20:23:25 +0900

    init [] 레파지토리 생성

---
+++ src/main/java/oop/lab/ooplab/OopLabApplication.java
@@ -0,0 +1,13 @@
+package oop.lab.ooplab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oop/lab/ooplab/OopLabApplicationTests.java
@@ -0,0 +1,13 @@
+package oop.lab.ooplab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
