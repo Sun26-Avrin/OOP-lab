81265d795743692372058c92be32af0c10cb836b
Ping9 <sim2626@naver.com>
2023-04-01 15:47:07 +0900

    task [요구사항 변경] 초기 설계시 객체에 송신하는 메세지를 `How` 에 초점을 두고 작성하였을 때

--- src/main/java/oop/lab/ooplab/message/focus/how/court/other/JobOfWitness.java
+++ src/main/java/oop/lab/ooplab/message/focus/how/court/other/JobOfWitness.java
@@ -1,5 +1,6 @@
 package oop.lab.ooplab.message.focus.how.court.other;
 
 public enum JobOfWitness {
-    TEACHER, STUDENT, SOCCER_PLAYER
+    TEACHER, STUDENT, SOCCER_PLAYER,
+    COVID_TEACHER
 }
---
+++ src/main/java/oop/lab/ooplab/message/focus/how/court/receptor/WitnessInfectedWithCovid19.java
@@ -0,0 +1,7 @@
+package oop.lab.ooplab.message.focus.how.court.receptor;
+
+import oop.lab.ooplab.message.focus.how.court.other.Testimony;
+
+public interface WitnessInfectedWithCovid19 extends Witness {
+    Testimony testifyTodayInWriting();
+}
